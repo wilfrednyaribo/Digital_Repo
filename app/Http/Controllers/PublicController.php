42a9a67a9d02bb5572abd3d5ccac234fc6a183a9
wilfrednyaribo <42717255+wilfrednyaribo@users.noreply.github.com>
2026-07-01 14:52:35 +0300

    interface redesigning

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -44,7 +44,7 @@ class PublicController extends Controller
             ->when(!$request->sort || $request->sort === 'newest', function ($query) {
                 $query->orderByDesc('created_at');
             })
-            ->paginate(200);
+            ->paginate(204);
 
         $categories = Category::orderBy('name')->get();
 
@@ -84,7 +84,7 @@ class PublicController extends Controller
             ->when(!$request->sort || $request->sort === 'newest', function ($query) {
                 $query->orderByDesc('created_at');
             })
-            ->paginate(200);
+            ->paginate(204);
 
         $categories = Category::orderBy('name')->get();
 
